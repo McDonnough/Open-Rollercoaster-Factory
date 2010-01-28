b68df2004c23e8e54068200b535f204478028e6c
Philip Rebohle <philip.rebohle@ewetel.net>
2010-01-28 18:57:07 +0100

    Modified a few shaders and added missing files -.-

--- data/rendereropengl/glsl/terrain/terrain.fs
+++ data/rendereropengl/glsl/terrain/terrain.fs
@@ -2,16 +2,14 @@
 
 uniform sampler2D TerrainTexture;
 
-varying float dist;
-
 vec2 trunc(vec2 a) {
   return a - floor(a);
 }
 
 vec2 getRightTexCoord(float fac) {
-  return trunc(gl_TexCoord[0].xy * 4.0 * fac) / 4.0;
+  return clamp(trunc(gl_TexCoord[0].xy * 4.0 * fac), 1.0 / 512.0, 1.0 - 1.0 / 512.0) / 4.0;
 }
 
 void main(void) {
-  gl_FragColor = texture2D(TerrainTexture, getRightTexCoord(1.0)) * vec4(1.0, 1.0, 1.0, dist - 10.0);
+  gl_FragColor = (texture2D(TerrainTexture, getRightTexCoord(1.0 / 483)) + texture2D(TerrainTexture, getRightTexCoord(1.0 / 128.0))) * 0.5;
 }

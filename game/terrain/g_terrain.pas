cd6b2215cffa3d0a8ccb084caad906b1a2b350e5
Philip Rebohle <philip.rebohle@ewetel.net>
2010-02-04 13:22:30 +0100

    Terrain scaling should work now

--- game/terrain/g_terrain.pas
+++ game/terrain/g_terrain.pas
@@ -18,17 +18,20 @@ type
       function GetHeightMap(X, Y: Single): Single;
       function GetTextureOffsetMap(X, Y: Single): TVector2D;
       function GetWaterMap(X, Y: Single): Single;
+      function GetSizeX: Word;
+      function GetSizeY: Word;
       procedure SetHeightMap(X, Y: Single; Z: Single);
       procedure SetTextureOffsetMap(X, Y: Single; Z: TVector2D);
       procedure SetWaterMap(X, Y: Single; Z: Single);
       procedure SetSizeX(X: Word);
       procedure SetSizeY(Y: Word);
     public
+      Multiplicator: Single;
       property HeightMap[X: Single; Y: Single]: Single read GetHeightMap write SetHeightMap;
       property TextureOffsetMap[X: Single; Y: Single]: TVector2D read GetTextureOffsetMap write SetTextureOffsetMap;
       property WaterMap[X: Single; Y: Single]: Single read GetWaterMap write SetWaterMap;
-      property SizeX: Word read fSizeX write SetSizeX;
-      property SizeY: Word read fSizeY write SetSizeY;
+      property SizeX: Word read GetSizeX write SetSizeX;
+      property SizeY: Word read GetSizeY write SetSizeY;
       property TextureCollectionName: String read fTextureCollectionName write SetTextureCollectionName;
       property Texture: TTexture read fTextureCollection;
       procedure WriteOCFSection(var Section: TOCFSection);
@@ -54,8 +57,8 @@ end;
 
 function TTerrain.GetHeightMap(X, Y: Single): Single;
 begin
-  X := Clamp(X, 0, fSizeX);
-  Y := Clamp(Y, 0, fSizeY);
+  X := Clamp(X / Multiplicator, 0, fSizeX);
+  Y := Clamp(Y / Multiplicator, 0, fSizeY);
   Result := Mix(Mix(fHeightMap[Floor(X) + (fSizeX + 1) * Floor(Y)], fHeightMap[Ceil(X) + (fSizeX + 1) * Floor(Y)], FPart(X)),
                 Mix(fHeightMap[Floor(X) + (fSizeX + 1) * Ceil(Y)], fHeightMap[Ceil(X) + (fSizeX + 1) * Ceil(Y)], FPart(X)),
                 fPart(Y)) / 256;
@@ -63,26 +66,36 @@ end;
 
 function TTerrain.GetTextureOffsetMap(X, Y: Single): TVector2D;
 begin
-  X := Clamp(Round(X), 0, fSizeX);
-  Y := Clamp(Round(Y), 0, fSizeY);
+  X := Clamp(Round(X / Multiplicator), 0, fSizeX);
+  Y := Clamp(Round(Y / Multiplicator), 0, fSizeY);
   Result := fTextureOffsetMap[Round(X + (fSizeX + 1) * Y)];
 end;
 
 function TTerrain.GetWaterMap(X, Y: Single): Single;
 begin
-  X := Clamp(X, 0, fSizeX);
-  Y := Clamp(Y, 0, fSizeY);
+  X := Clamp(X / Multiplicator, 0, fSizeX);
+  Y := Clamp(Y / Multiplicator, 0, fSizeY);
   Result := Mix(Mix(fWaterMap[Floor(X) + (fSizeX + 1) * Floor(Y)], fWaterMap[Ceil(X) + (fSizeX + 1) * Floor(Y)], FPart(X)),
                 Mix(fWaterMap[Floor(X) + (fSizeX + 1) * Ceil(Y)], fWaterMap[Ceil(X) + (fSizeX + 1) * Ceil(Y)], FPart(X)),
                 fPart(Y)) / 256;
+end;
+
+function TTerrain.GetSizeX: Word;
+begin
+  Result := Round(fSizeX * Multiplicator);
+end;
+
+function TTerrain.GetSizeY: Word;
+begin
+  Result := Round(fSizeY * Multiplicator);
 end;
 
 procedure TTerrain.SetHeightMap(X, Y: Single; Z: Single);
 var
   WX, WY, WZ: Word;
 begin
-  X := Clamp(X, 0, fSizeX);
-  Y := Clamp(Y, 0, fSizeY);
+  X := Clamp(X / Multiplicator, 0, fSizeX);
+  Y := Clamp(Y / Multiplicator, 0, fSizeY);
   WZ := Round(256 * Z);
   WX := Round(X);
   WY := Round(Y);
@@ -91,8 +104,8 @@ end;
 
 procedure TTerrain.SetTextureOffsetMap(X, Y: Single; Z: TVector2D);
 begin
-  X := Clamp(Round(X), 0, fSizeX);
-  Y := Clamp(Round(Y), 0, fSizeY);
+  X := Clamp(Round(X / Multiplicator), 0, fSizeX);
+  Y := Clamp(Round(Y / Multiplicator), 0, fSizeY);
   fTextureOffsetMap[Round(X + (fSizeX + 1) * Y)] := Z;
 end;
 
@@ -111,6 +124,7 @@ var
   i, j: Integer;
   CP: Integer;
 begin
+  X := Round(X / Multiplicator);
   if X = fSizeX then
     exit
   else if X > fSizeX then
@@ -158,6 +172,7 @@ procedure TTerrain.SetSizeY(Y: Word);
 var
   i: integer;
 begin
+  Y := Round(Y / Multiplicator);
   SetLength(fHeightMap, (fSizeX + 1) * (Y + 1));
   SetLength(fWaterMap, (fSizeX + 1) * (Y + 1));
   SetLength(fTextureOffsetMap, (fSizeX + 1) * (Y + 1));
@@ -179,7 +194,11 @@ begin
   tmpw := Length(fTextureCollectionName);
   Section.Data.CopyFromByteArray(@tmpW, Sizeof(Word));
   Section.Data.AppendByteArray(@fTextureCollectionName[0], tmpW);
-  Section.Data.AppendByteArray(@fSizeX, 2 * Sizeof(Word));
+  Section.Data.AppendByteArray(@Multiplicator, Sizeof(Single));
+  tmpW := SizeX;
+  Section.Data.AppendByteArray(@tmpW, Sizeof(Word));
+  tmpW := SizeY;
+  Section.Data.AppendByteArray(@tmpW, Sizeof(Word));
   Section.Data.AppendByteArray(@fHeightMap[0], (fSizeX + 1) * (fSizeY + 1) * Sizeof(Word));
   Section.Data.AppendByteArray(@fWaterMap[0], (fSizeX + 1) * (fSizeY + 1) * Sizeof(Word));
   Section.Data.AppendByteArray(@fTextureOffsetMap[0], (fSizeX + 1) * (fSizeY + 1) * Sizeof(TVector2D));
@@ -193,6 +212,7 @@ begin
   SetLength(fTextureCollectionName, tmpW);
   Section.Data.ReadBytes(@fTextureCollectionName[1], tmpW);
   TextureCollectionName := fTextureCollectionName;
+  Section.Data.ReadBytes(@Multiplicator, SizeOf(Single));
   Section.Data.ReadBytes(@tmpW, SizeOf(Word));
   SizeX := tmpW;
   Section.Data.ReadBytes(@tmpW, SizeOf(Word));
@@ -206,6 +226,7 @@ end;
 procedure TTerrain.LoadDefaults;
 begin
   if fLoaded then exit;
+  Multiplicator := 2;
   SizeX := 256;
   SizeY := 256;
   TextureCollectionName := 'terrain/defaultcollection.tga';
--- modules/renderer/opengl/m_renderer_opengl_terrain.pas
+++ modules/renderer/opengl/m_renderer_opengl_terrain.pas
@@ -9,7 +9,6 @@ type
   TRTerrain = class
     protected
       fVBO: TVBO;
-      fHeightMap: TTexture;
       fShader: TShader;
     public
       procedure Render;
@@ -26,7 +25,6 @@ procedure TRTerrain.Render;
 var
   i, j: Integer;
 begin
-  fHeightMap.Bind(1);
   Park.pTerrain.Texture.Bind(0);
 
   glColor4f(1, 1, 1, 1);
@@ -39,70 +37,47 @@ end;
 constructor TRTerrain.Create;
 var
   i, j, k: Integer;
-  hm: array of DWord;
 begin
   fShader := TShader.Create('rendereropengl/glsl/terrain/terrain.vs', 'rendereropengl/glsl/terrain/terrain.fs');
   fShader.UniformI('TerrainTexture', 0);
-  fShader.UniformI('HeightMap', 1);
-  fShader.UniformF('TerrainSize', Park.pTerrain.SizeX, Park.pTerrain.SizeY);
 
-  fHeightMap := TTexture.Create;
-  fHeightMap.SetClamp(GL_CLAMP, GL_CLAMP);
-  fHeightMap.CreateNew(Park.pTerrain.SizeX + 1, Park.pTerrain.SizeY + 1, GL_RGBA32F_ARB);
-
-  fVBO := TVBO.Create(Park.pTerrain.SizeX * Park.pTerrain.SizeY * 4, GL_T2F_C4F_N3F_V3F, GL_QUADS);
+  fVBO := TVBO.Create(Round(Park.pTerrain.SizeX / Park.pTerrain.Multiplicator) * Round(Park.pTerrain.SizeY / Park.pTerrain.Multiplicator) * 4, GL_T2F_C4F_N3F_V3F, GL_QUADS);
   j := 0;
   k := 0;
-  setlength(hm, (Park.pTerrain.SizeX + 1) * (Park.pTerrain.SizeY + 1));
-  for i := 0 to Park.pTerrain.SizeX * Park.pTerrain.SizeY - 1 do
+  Park.pTerrain.HeightMap[10, 10] := 10;
+  for i := 0 to Round(Park.pTerrain.SizeX / Park.pTerrain.Multiplicator) * Round(Park.pTerrain.SizeY / Park.pTerrain.Multiplicator) - 1 do
     begin
-    fVBO.TexCoords[4 * i] := Vector(j / 16, k / 32);
-    fVBO.Vertices[4 * i] := Vector(j, 0, k);
-    fVBO.Normals[4 * i] := Vector(0, 1, 0);
-    fVBO.Colors[4 * i] := Vector(0, 0, 0, 0);
-
-    fVBO.TexCoords[4 * i + 1] := Vector((j + 1) / 16, k / 32);
-    fVBO.Vertices[4 * i + 1] := Vector(j + 1, 0, k);
-    fVBO.Normals[4 * i + 1] := Vector(0, 1, 0);
-    fVBO.Colors[4 * i + 1] := Vector(0, 0, 0, 0);
-
-    fVBO.TexCoords[4 * i + 2] := Vector((j + 1) / 16, (k + 1) / 32);
-    fVBO.Vertices[4 * i + 2] := Vector(j + 1, 0, k + 1);
-    fVBO.Normals[4 * i + 2] := Vector(0, 1, 0);
-    fVBO.Colors[4 * i + 2] := Vector(0, 0, 0, 0);
-
-    fVBO.TexCoords[4 * i + 3] := Vector(j / 16, (k + 1) / 32);
-    fVBO.Vertices[4 * i + 3] := Vector(j, 0, k + 1);
+    fVBO.TexCoords[4 * i + 3] := Vector(Park.pTerrain.Multiplicator * j / 16, Park.pTerrain.Multiplicator * k / 32);
+    fVBO.Vertices[4 * i + 3] := Vector(Park.pTerrain.Multiplicator * j, Park.pTerrain.HeightMap[Park.pTerrain.Multiplicator * j, Park.pTerrain.Multiplicator * k], Park.pTerrain.Multiplicator * k);
     fVBO.Normals[4 * i + 3] := Vector(0, 1, 0);
     fVBO.Colors[4 * i + 3] := Vector(0, 0, 0, 0);
 
+    fVBO.TexCoords[4 * i + 2] := Vector(Park.pTerrain.Multiplicator * (j + 1) / 16, Park.pTerrain.Multiplicator * k / 32);
+    fVBO.Vertices[4 * i + 2] := Vector(Park.pTerrain.Multiplicator * (j + 1), Park.pTerrain.HeightMap[Park.pTerrain.Multiplicator * (j + 1), Park.pTerrain.Multiplicator * k], Park.pTerrain.Multiplicator * k);
+    fVBO.Normals[4 * i + 2] := Vector(0, 1, 0);
+    fVBO.Colors[4 * i + 2] := Vector(0, 0, 0, 0);
+
+    fVBO.TexCoords[4 * i + 1] := Vector(Park.pTerrain.Multiplicator * (j + 1) / 16, Park.pTerrain.Multiplicator * (k + 1) / 32);
+    fVBO.Vertices[4 * i + 1] := Vector(Park.pTerrain.Multiplicator * (j + 1), Park.pTerrain.HeightMap[Park.pTerrain.Multiplicator * (j + 1), Park.pTerrain.Multiplicator * (k + 1)], Park.pTerrain.Multiplicator * (k + 1));
+    fVBO.Normals[4 * i + 1] := Vector(0, 1, 0);
+    fVBO.Colors[4 * i + 2] := Vector(0, 0, 0, 0);
+
+    fVBO.TexCoords[4 * i] := Vector(Park.pTerrain.Multiplicator * j / 16, Park.pTerrain.Multiplicator * (k + 1) / 32);
+    fVBO.Vertices[4 * i] := Vector(Park.pTerrain.Multiplicator * j, Park.pTerrain.HeightMap[Park.pTerrain.Multiplicator * j, Park.pTerrain.Multiplicator * (k + 1)], Park.pTerrain.Multiplicator * (k + 1));
+    fVBO.Normals[4 * i] := Vector(0, 1, 0);
+    fVBO.Colors[4 * i] := Vector(0, 0, 0, 0);
+
     inc(j);
-    if j = Park.pTerrain.SizeX then
+    if j = Round(Park.pTerrain.SizeX / Park.pTerrain.Multiplicator) then
       begin
       j := 0;
       inc(k);
       end;
     end;
-
-  j := 0;
-  k := 0;
-  for i := 0 to high(hm) do
-    begin
-    hm[i] := Round(256 * Park.pTerrain.HeightMap[j, k]);
-    inc(j);
-    if j = Park.pTerrain.SizeX then
-      begin
-      j := 0;
-      inc(k);
-      end;
-    end;
-
-  fHeightMap.Fill(@hm[0], GL_RGBA);
 end;
 
 destructor TRTerrain.Free;
 begin
-  fHeightMap.Free;
   fShader.Free;
   fVBO.Free;
 end;

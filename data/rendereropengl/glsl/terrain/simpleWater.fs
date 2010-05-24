64dba1d2832687df666d07ca00a0397583598b4f
Philip Rebohle <philip.rebohle@ewetel.net>
2010-05-24 13:31:19 +0200

    missing shader file added

---
+++ data/rendereropengl/glsl/terrain/simpleWater.fs
@@ -0,0 +1,24 @@
+#version 120
+
+uniform sampler2D HeightMap;
+uniform vec2 TerrainSize;
+
+varying vec4 Vertex;
+
+float fpart(float a) {
+  return a - floor(a);
+}
+
+float fetchHeight(int index) {
+  return mix(
+          mix(texture2D(HeightMap, 5.0 * (Vertex.xz  + vec2(0.0, 0.0)) / TerrainSize)[index], texture2D(HeightMap, 5.0 * (Vertex.xz  + vec2(0.2, 0.0)) / TerrainSize)[index], fpart(5.0 * Vertex.x)),
+          mix(texture2D(HeightMap, 5.0 * (Vertex.xz  + vec2(0.0, 0.2)) / TerrainSize)[index], texture2D(HeightMap, 5.0 * (Vertex.xz  + vec2(0.2, 0.2)) / TerrainSize)[index], fpart(5.0 * Vertex.x)),
+          fpart(5.0 * Vertex.z)) * 256.0;
+}
+
+void main(void) {
+  float h = fetchHeight(1);
+  if (h < Vertex.y - 0.01 || h > Vertex.y + 0.01)
+    discard;
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}

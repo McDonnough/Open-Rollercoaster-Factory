96cba9dd9b97aa38e00b64d1954f3e770510e2d9
Philip Rebohle <philip@philip-desktop.localdomain>
2010-07-10 16:35:30 +0200

    backup restored

--- utils/u_xml.pas
+++ utils/u_xml.pas
@@ -247,7 +247,7 @@ var
   end;
 begin
   s := '';
-  Result := Result + '<?xml version="1.0" encoding="UTF-8"?>' + #10;
+  Result := '<?xml version="1.0" encoding="UTF-8"?>' + #10;
   if fDocument.FirstChild <> nil then
     AddChildrenRecursively(fDocument.FirstChild);
 end;

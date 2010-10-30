de01f91686753d401f521dc539da7e063c35e0eb
Philip Rebohle <philip.rebohle@ewetel.net>
2010-10-30 16:35:03 +0200

    First post-processor: motion blur

--- data/orcf-world-engine/scene/terrain/terrain.fs
+++ data/orcf-world-engine/scene/terrain/terrain.fs
@@ -5,6 +5,25 @@ uniform sampler2D TerrainTexture;
 uniform float TerrainTesselationDistance;
 uniform float HeightLine;
 
+varying vec3 Vertex;
+
+float fetchHeightAtOffset(vec2 O) {
+  float result = mix(
+          mix(texture2D(HeightMap, 5.0 * (Vertex.xz + O + vec2(0.0, 0.0)) / TerrainSize).a, texture2D(HeightMap, 5.0 * (Vertex.xz + O + vec2(0.2, 0.0)) / TerrainSize).a, fpart(5.0 * Vertex.x)),
+          mix(texture2D(HeightMap, 5.0 * (Vertex.xz + O + vec2(0.0, 0.2)) / TerrainSize).a, texture2D(HeightMap, 5.0 * (Vertex.xz + O + vec2(0.2, 0.2)) / TerrainSize).a, fpart(5.0 * Vertex.x)),
+          fpart(5.0 * Vertex.z)) * 256.0;
+  result = mix(64.0, result, (0.5 - 0.5 * cos(3.141 * pow(1.0 - rhf, 5.0))));
+  return result;
+}
+
 void main(void) {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  vec3 normal = normalize(
+    normalize(cross(vec3(+0.0, fetchHeightAtOffset(vec2(+ 0.0, - 0.2)) - VY, -0.2), vec3(-0.2, fetchHeightAtOffset(vec2(- 0.2, + 0.0)) - VY, +0.0)))
+  + normalize(cross(vec3(+0.2, fetchHeightAtOffset(vec2(+ 0.2, + 0.0)) - VY, +0.0), vec3(+0.0, fetchHeightAtOffset(vec2(+ 0.0, - 0.2)) - VY, -0.2)))
+  + normalize(cross(vec3(+0.0, fetchHeightAtOffset(vec2(+ 0.0, + 0.2)) - VY, +0.2), vec3(+0.2, fetchHeightAtOffset(vec2(+ 0.2, + 0.0)) - VY, -0.0)))
+  + normalize(cross(vec3(-0.2, fetchHeightAtOffset(vec2(- 0.2, + 0.0)) - VY, +0.0), vec3(+0.0, fetchHeightAtOffset(vec2(+ 0.0, + 0.2)) - VY, +0.2))));
+  gl_FragData[0].rgb = Vertex;
+  gl_FragData[0].a = length(gl_ModelViewMatrix * vec4(Vertex, 1.0));
+  gl_FragData[1] = vec4(normal, 1.0);
+  gl_FragData[2] = vec4(1.0, 1.0, 1.0, 1.0);
 }

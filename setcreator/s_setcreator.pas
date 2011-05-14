1648a36a789b31e0e2afbac6fe796f9c8e46d739
Philip Rebohle <philip.rebohle@ewetel.net>
2011-05-14 13:57:26 +0200

    added empty set creator file

---
+++ setcreator/s_setcreator.pas
@@ -0,0 +1,11 @@
+unit s_setcreator;
+
+interface
+
+uses
+  SysUtils, Classes, m_gui_class, m_gui_window_class, m_gui_button_class, m_gui_edit_class, m_gui_iconifiedbutton_class, m_gui_label_class,
+  m_gui_scrollbox_class, m_gui_slider_class, m_gui_progressbar_class, m_gui_checkbox_class;
+
+implementation
+
+end.
